e27777df76ec2b1af8c51f8069085fbc98ce856f
Davinguer <limadavilima2005@gmail.com>
2023-05-02 12:32:01 -0300

    Upgrade de style

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path =  require('path');
 
 const app = express();
 
-const indexRouter = require('./src/routes/index');
+const indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
 
