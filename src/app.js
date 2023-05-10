65f4c300a1eaef95a7b85ecde49b21dd2672f5ff
Bonniezilla <bonnieevil86@gmail.com>
2023-05-10 10:58:25 -0300

    Fazendo a conexão com db e autentificação de usuário

--- src/app.js
+++ src/app.js
@@ -2,30 +2,57 @@ const express = require('express');
 const session = require('express-session');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
-const path =  require('path');
-
-var name = 'Pedro';
-var password = '123';
+const path = require('path');
+const { client, connection } = require('./connection');
 
 const port = 3000;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({secret:'timmaianao'}));
+var username = 'Pedro';
+var password = '123';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: '123456',
+    resave: false,
+    saveUninitialized: false
+}));
 
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
 
-app.post('/', (req, res) => {
-    if(req.body.password === password && req.body.name === name) {
-        res.render('index');
-    } else {
-        res.render('login');
-    }
+app.post('/login', (req, res) => {
+    connection.init();
+
+    const { username, password } = req.body;
+
+    const query = "SELECT * FROM usuarios WHERE username = $1 AND password = $2";
+
+    client.query(query, [username, password], (err, result) => {
+        connection.destroy();
+
+        if (err) {
+            console.error('Ocorreu um erro', err);
+            return res.redirect('/');
+        }
+
+        if (result.rowCount === 0) {
+            console.log('Crendenciais inválidas');
+            return res.redirect('/');
+        }
+        
+        console.log(`${username} foi logado com sucesso!`)
+        req.session.login = true;
+        return res.redirect('/');
+    })
+});
+
+app.get('/login', (req, res) => {
+    res.redirect('/');
 });
 
 app.get('/', (req, res) => {
-    if(req.session.login) {
+    if (req.session.login) {
         res.render('index');
     } else {
         res.render('login');
