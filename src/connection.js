6c7d5af2286e9e51e741a2681701a04561f4b19a
Bonniezilla <bonnieevil86@gmail.com>
2023-05-11 19:05:02 -0300

    Consertando a conexão com db do Render

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,8 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    user: 'jornal_dr_pedro_db_user',
-    host: 'postgres://jornal_dr_pedro_db_user:ywqkH4IMeCsa0r7Oc19rGTews4uVwHTn@dpg-ch8hqttgk4q7lmqj4ifg-a/jornal_dr_pedro_db',
-    database: 'jornal_dr_pedro_db',
-    password: 'ywqkH4IMeCsa0r7Oc19rGTews4uVwHTn',
-    port: 5432
+    connectionString: "postgres://jornal_dr_pedro_db_user:ywqkH4IMeCsa0r7Oc19rGTews4uVwHTn@dpg-ch8hqttgk4q7lmqj4ifg-a.ohio-postgres.render.com/jornal_dr_pedro_db",
+    ssl: true
 });
 
 const connection = {
