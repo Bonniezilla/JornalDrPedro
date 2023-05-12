b8e42209ece669f415b0bc4562b601574f202f10
Bonniezilla <bonnieevil86@gmail.com>
2023-05-11 21:10:19 -0300

    Adicionando o dotenv para ler as variáves de ambiente de desenvolvimento

--- src/connection.js
+++ src/connection.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL || "postgres://jornal_dr_pedro_db_user:ywqkH4IMeCsa0r7Oc19rGTews4uVwHTn@dpg-ch8hqttgk4q7lmqj4ifg-a.ohio-postgres.render.com/jornal_dr_pedro_db",
+    connectionString: process.env.DB_URL,
     ssl: true
 });
 
