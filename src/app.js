d9410dca9d472c50c6f013b8f0cc82503ce60568
Bonniezilla <bonnieevil86@gmail.com>
2023-05-15 09:24:02 -0300

    Colocando prisma e fazendo autentificação de usuário com ele

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const session = require('express-session');
 const ejs = require('ejs');
-const bodyParser = require('body-parser');
 const path = require('path');
-const { client, connection } = require('./connection');
+const bodyParser = require('body-parser');
 
 const port = 3000;
 const app = express();
-
-var username = 'Pedro';
-var password = '123';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
@@ -20,48 +16,14 @@ app.use(session({
 
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
+const logupRouter = require('./routes/logup');
 
-app.post('/login', (req, res) => {
-    connection.init();
+app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/logup', logupRouter);
 
-    const { username, password } = req.body;
-
-    const query = "SELECT * FROM usuarios WHERE username = $1 AND password = $2";
-
-    client.query(query, [username, password], (err, result) => {
-        connection.destroy();
-
-        if (err) {
-            console.error('Ocorreu um erro', err);
-            return res.redirect('/');
-        }
-
-        if (result.rowCount === 0) {
-            console.log('Crendenciais inválidas');
-            return res.redirect('/');
-        }
-        
-        console.log(`${username} foi logado com sucesso!`)
-        req.session.login = true;
-        return res.redirect('/');
-    })
-});
-
-app.get('/login', (req, res) => {
-    res.redirect('/');
-});
-
-app.get('/', (req, res) => {
-    if (req.session.login) {
-        res.render('index');
-    } else {
-        res.render('login');
-    }
-});
-
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(process.env.PORT || port, () => {
--- src/connection.js
+++ src/connection.js
@@ -1,37 +1,7 @@
-const { Client } = require('pg');
+const { PrismaClient } = require('@prisma/client');
 
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'JornalDev',
-    password: 'borespedro1',
-    port: 5473
-});
-
-const connection = {
-    init: () => {
-        client.connect()
-        .then(() => {
-            console.log('Conexão estabelecida com sucesso');
-        })
-        .catch((err) => {
-            console.error('Ocorreu um erro:', err);
-        })
-    },
-    destroy: () => {
-        client.end()
-        .then(() => {
-            console.log('Conexão encerrada com sucesso!');
-        })
-        .catch((err) => {
-            console.error('Ocorreu um erro:', err);
-        })
-    }
-}
-
-
+const prisma = new PrismaClient();
 
 module.exports = {
-    client,
-    connection
+    prisma
 };
